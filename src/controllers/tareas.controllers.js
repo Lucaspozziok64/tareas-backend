4f744017ecf531029cc04f74067976c078616756
Lucasfi <lukafigueroa64@gmail.com>
2025-08-18 11:54:46 -0300

    agregamos funcion de crear tarea e hicimos prueba con postman

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,5 +1,20 @@
+import Tarea from "../models/tarea.js";
 
 export const test = (req, res) => {
     res.status(200);
     res.send('Primera prueba desde el backend');
+}
+
+export const crearTarea = async (req, res) => {
+    try {
+        console.log(req.body)
+
+        const nuevaTarea = new Tarea(req.body)
+        await nuevaTarea.save()
+
+        res.status(200).json({ mensjae: 'La tarea fue creada exitosamente' });
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ mensjae: 'Error al crear la tarea' });
+    }
 }
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { test } from "../controllers/tareas.controllers.js";
+import { test, crearTarea } from "../controllers/tareas.controllers.js";
 
 const router = new Router()
 
 router.route('/test').get(test)
+router.route('/').post(crearTarea)
 
 export default router;
