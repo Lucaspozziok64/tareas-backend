d14e688c06e486776b263e14e178deca86ae4fdc
Lucasfi <lukafigueroa64@gmail.com>
2025-08-20 12:15:22 -0300

    Agregue logica para obtener las tareas creadas

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -18,3 +18,14 @@ export const crearTarea = async (req, res) => {
         res.status(500).json({ mensjae: 'Error al crear la tarea' });
     }
 }
+
+export const leerTareas = async (req, res) => {
+    try {
+        const listaTareas = await Tarea.find()
+
+        res.status(200).json(listaTareas)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({mensaje: 'Error al leer los productos' })
+    }
+}
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { test, crearTarea } from "../controllers/tareas.controllers.js";
+import { test, crearTarea, leerTareas } from "../controllers/tareas.controllers.js";
 
 const router = new Router()
 
 router.route('/test').get(test)
-router.route('/').post(crearTarea)
+router.route('/').post(crearTarea).get(leerTareas)
 
 export default router;
