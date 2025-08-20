36e6d35cc18a554683a39beb87ae1210a590dcb2
Lucasfi <lukafigueroa64@gmail.com>
2025-08-20 12:51:25 -0300

    agregamos logica para eliminar tareas por id

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -43,3 +43,16 @@ export const leerTareasPorId = async (req, res) => {
         res.status(500).json({ mensaje: 'Error al obtener la tarea' })
     }
 }
+
+export const eliminarTareaPorId = async (req, res) => {
+    try {
+        const tareaEliminada = await Tarea.findByIdAndDelete(req.params.id);
+        if(!tareaEliminada) {
+            return res.status(404).json({ mensjae: 'Tarea no encontrada' })
+        }
+        res.status(200).json({ mensaje: 'Tarea eliminada exitosamente' });
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ mensaje: 'Error al eliminar la tarea' })
+    }
+}
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { test, crearTarea, leerTareas, leerTareasPorId } from "../controllers/tareas.controllers.js";
+import { test, crearTarea, leerTareas, leerTareasPorId, eliminarTareaPorId } from "../controllers/tareas.controllers.js";
 
 const router = new Router()
 
 router.route('/test').get(test)
 router.route('/').post(crearTarea).get(leerTareas)
-router.route('/:id').get(leerTareasPorId)
+router.route('/:id').get(leerTareasPorId).delete(eliminarTareaPorId)
 
 export default router;
